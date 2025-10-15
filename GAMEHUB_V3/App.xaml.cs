d4cc176aa43acd39a8755b8c0fad2342d8bead31
Oswaldo <88911060+Oswbr7@users.noreply.github.com>
2025-10-15 17:25:38 -0600

    V1

--- GAMEHUB_V3/App.xaml.cs
+++ GAMEHUB_V3/App.xaml.cs
@@ -4,9 +4,7 @@ using System.Windows;
 
 namespace GAMEHUB_V3
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : System.Windows.Application
     {
     }
